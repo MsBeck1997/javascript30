e84aacc94cfbf5b02b1e0543c7be32b10b557579
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-03-30 11:47:07 -0600

    added script.js and added exercises for ch. 2

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+// Looping Triangle Exercise - Chapter 2 JavaScript
+for (var symbol = "#"; symbol.length <= 7; symbol = symbol + "#") {
+	console.log(symbol)
+}
+*/
+
+/*
+// FizzBuzz Exercise - Chapter 2 Javascript
+for (number = 0; number <= 100; number = number + 1) {
+	if (number % 15 == 0) {
+		console.log("FizzBuzz")
+	}
+	else if (number % 3 == 0) {
+		console.log("Fizz")
+	}
+	else if (number % 5 == 0) {
+		console.log("Buzz")
+	}
+	else {
+		console.log(number)
+	}
+}
+ */
+
+// Chessboard Exercise - Chapter 2 JavaScript exercise
